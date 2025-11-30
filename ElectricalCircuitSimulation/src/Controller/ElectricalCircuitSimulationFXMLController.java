e3a7a825f8cdb722df3cb79ef6075ea773ec1163
Lucas-Dash76 <lukedash02@gmail.com>
2025-11-30 17:44:41 -0500

    custome clear btn implenmentation

--- ElectricalCircuitSimulation/src/Controller/ElectricalCircuitSimulationFXMLController.java
+++ ElectricalCircuitSimulation/src/Controller/ElectricalCircuitSimulationFXMLController.java
@@ -9,11 +9,14 @@ import Model.CircuitElement;
 import Model.CircuitSplit;
 import Model.Resistor;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -24,6 +27,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
 
 /**
  * FXML Controller class
@@ -61,10 +65,7 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
     private Label rs_R2_V;
     @FXML
     private Label rs_R2_C;
-    @FXML
-    private Label rs_TotalCurrent;
-    @FXML
-    private Label rs_EquivResistance;
+    
     @FXML
     private Button rs_calcBtn;
     @FXML
@@ -89,10 +90,7 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
     private Label rp_R2_V;
     @FXML
     private Label rp_R2_C;
-    @FXML
-    private Label rp_TotalCurrent;
-    @FXML
-    private Label rp_EquivResistance;
+  
     @FXML
     private Button rp_calcBtn;
     @FXML
@@ -117,10 +115,8 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
     private Label cs_C2_V;
     @FXML
     private Label cs_C2_C;
-    @FXML
-    private Label cs_TotalCharge;
-    @FXML
-    private Label cs_EquivCapacitance;
+ 
+ 
     @FXML
     private Button cs_calcBtn;
     @FXML
@@ -145,10 +141,7 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
     private Label cp_C2_V;
     @FXML
     private Label cp_C2_C;
-    @FXML
-    private Label cp_TotalCharge;
-    @FXML
-    private Label cp_EquivCapacitance;
+
     @FXML
     private Button cp_calcBtn;
     @FXML
@@ -158,13 +151,10 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
     @FXML
     private TextField b5_tf;
     @FXML
-    private ComboBox<?> rc_comboBox;
+    private ComboBox<String> rc_comboBox;
     @FXML
     private Button rc_addBtn;
-    @FXML
-    private Label rc_TotalCurrent;
-    @FXML
-    private Label rc_EquivResistance;
+ 
     @FXML
     private Button rc_calcBtn;
     @FXML
@@ -174,13 +164,10 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
     @FXML
     private TextField b6_tf;
     @FXML
-    private ComboBox<?> cc_comboBox;
+    private ComboBox<String> cc_comboBox;
     @FXML
     private Button cc_addBtn;
-    @FXML
-    private Label cc_TotalCharge;
-    @FXML
-    private Label cc_EquivCapacitance;
+    
     @FXML
     private Button cc_calcBtn;
     @FXML
@@ -189,14 +176,32 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
     private VBox resistorVbox;
     @FXML
     private VBox rcSidebarVbox;
-    private int counter;
+    private int counter1;
+    private int counter2;
+    private List<VBox> rcvbs = new ArrayList<>();
+    private List<VBox> ccvbs = new ArrayList<>();
+    
+    @FXML
+    private VBox capacitorvb;
+    @FXML
+    private VBox ccSidebarVbox;
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        counter = 0;
+        counter1 = 0;
+        counter2 = 0;
+        cc_comboBox.getItems().addAll(
+        "Branch","Capacitor"
+    );
+        rc_comboBox.getItems().addAll(
+        "Branch","Resistor"
+    );
+        cc_comboBox.setValue("Branch");
+        rc_comboBox.setValue("Branch");
+        
         
         bindSliderToTextField(b1_slider, b1_tf);
         bindSliderToTextField(rs_R1_slider, rs_R1_tf);
@@ -214,16 +219,25 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
         bindSliderToTextField(cp_C1_slider, cp_C1_tf);
         bindSliderToTextField(cp_C2_slider, cp_C2_tf);
         
+        bindSliderToTextField(b5_slider, b5_tf);
     }
 
+    /**
+     * resets all the variables in tab 1
+     * @param event 
+     */
     @FXML
     private void rsClearBtnPressed(ActionEvent event) {
         b1_slider.setValue(20);
-        b1_tf.setText(null);
+        b1_tf.setText("10.00");
         rs_R1_slider.setValue(20);
-        rs_R1_tf.setText(null);
+        rs_R1_tf.setText("10.00");
         rs_R2_slider.setValue(20);
-        rs_R2_tf.setText(null);
+        rs_R2_tf.setText("10.00");
+        rs_R1_V.setText("Voltage: 0");
+        rs_R2_V.setText("Voltage: 0");
+        rs_R1_C.setText("Current: 0");
+        rs_R2_C.setText("Current: 0");
     }
 
 
@@ -231,31 +245,101 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
 
     
 
-
+     /**
+     * resets all the variables in tab 2
+     * @param event 
+     */
     @FXML
     private void rpClearBtnPressed(ActionEvent event) {
         b2_slider.setValue(20);
-        b2_tf.setText(null);
+        b2_tf.setText("10.00");
         rp_R1_slider.setValue(20);
-        rp_R1_tf.setText(null);
+        rp_R1_tf.setText("10.00");
         rp_R2_slider.setValue(20);
-        rp_R2_tf.setText(null);
+        rp_R2_tf.setText("10.00");
+        rp_R1_V.setText("Voltage: 0");
+        rp_R2_V.setText("Voltage: 0");
+        rp_R1_C.setText("Current: 0");
+        rp_R2_C.setText("Current: 0");
     }
 
+    /**
+     * enters the calculated voltage and current into tab 1
+     * @param event 
+     */
     @FXML
     private void rsCalcBtnPreesed(ActionEvent event) {
+
+        CircuitElement[] calcCircuit = {
+            new Battery(Double.parseDouble(b1_tf.getText())),
+            new Resistor(Double.parseDouble(rs_R1_tf.getText()), 0, 0),
+            new Resistor(Double.parseDouble(rs_R2_tf.getText()), 0, 0),
+        };
+        calcResistance(calcCircuit, calcCircuit.length, 0, false, 0);
+        rs_R1_V.setText("Voltage: " + calcCircuit[1].getVoltage());
+        rs_R1_C.setText("Current: " + ((Resistor)calcCircuit[1]).getCurrent());
+        rs_R2_V.setText("Voltage: " + calcCircuit[2].getVoltage());
+        rs_R2_C.setText("Current: " + ((Resistor)calcCircuit[2]).getCurrent());
+        
     }
 
+    /**
+     * enters the calculated voltage and current into tab 2
+     * @param event 
+     */
     @FXML
     private void rpCalcBtnPressed(ActionEvent event) {
+        CircuitElement[] calcCircuit = {
+            new Battery(Double.parseDouble(b2_tf.getText())),
+            new Resistor(Double.parseDouble(rp_R1_tf.getText()), 0, 0),
+            new Resistor(Double.parseDouble(rp_R2_tf.getText()), 0, 0),
+        };
+        //calcResistance(calcCircuit, calcCircuit.length, 0, false, 0);
+        rp_R1_V.setText("Voltage: " + calcCircuit[1].getVoltage());
+        rp_R1_C.setText("Current: " + ((Resistor)calcCircuit[1]).getCurrent());
+        rp_R2_V.setText("Voltage: " + calcCircuit[2].getVoltage());
+        rp_R2_C.setText("Current: " + ((Resistor)calcCircuit[2]).getCurrent());
+        
+    
     }
     
+    /**
+     * enters the calculated voltage and charge into tab 3
+     * @param event 
+     */
     @FXML
     private void cpCalcBtnPressed(ActionEvent event) {
+        CircuitElement[] calcCircuit = {
+            new Battery(Double.parseDouble(b3_tf.getText())),
+            new Resistor(Double.parseDouble(cs_C1_tf.getText()), 0, 0),
+            new Resistor(Double.parseDouble(cs_C2_tf.getText()), 0, 0),
+        };
+        //calcResistance(calcCircuit, calcCircuit.length, 0, false, 0);
+        //cs_C1_V.setText("Voltage: " + calcCircuit[1].getVoltage());
+        //cs_C1_C.setText("Charge: " + ((Capacitor)calcCircuit[1]).getCurrent());
+        //cs_C2_V.setText("Voltage: " + calcCircuit[2].getVoltage());
+        //cs_C2_C.setText("Charge: " + ((Resistor)calcCircuit[2]).getCurrent());
+        
+    
     }
     
+    /**
+     * enters the calculated voltage and charge into tab 4
+     * @param event 
+     */
     @FXML
     private void csCalcBtnPressed(ActionEvent event) {
+        CircuitElement[] calcCircuit = {
+            new Battery(Double.parseDouble(b4_tf.getText())),
+            new Resistor(Double.parseDouble(cp_C1_tf.getText()), 0, 0),
+            new Resistor(Double.parseDouble(cp_C2_tf.getText()), 0, 0),
+        };
+        //calcResistance(calcCircuit, calcCircuit.length, 0, false, 0);
+        //cp_C1_V.setText("Voltage: " + calcCircuit[1].getVoltage());
+        //cp_C1_C.setText("Charge: " + ((Resistor)calcCircuit[1]).getCurrent());
+        //cp_C2_V.setText("Voltage: " + calcCircuit[2].getVoltage());
+        //cp_C2_C.setText("Charge: " + ((Resistor)calcCircuit[2]).getCurrent());
+        
     }
     
 
@@ -264,14 +348,22 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
 
 
 
+    /**
+     * resets all the variables in tab 3
+     * @param event 
+     */
     @FXML
     private void csClearBtnPressed(ActionEvent event) {
         b3_slider.setValue(20);
-        b3_tf.setText(null);
+        b3_tf.setText("10.00");
         cs_C1_slider.setValue(20);
-        cs_C1_tf.setText(null);
+        cs_C1_tf.setText("10.00");
         cs_C2_slider.setValue(20);
-        cs_C2_tf.setText(null);
+        cs_C2_tf.setText("10.00");
+        cs_C1_V.setText("Voltage: 0");
+        cs_C2_V.setText("Voltage: 0");
+        cs_C1_C.setText("Charge: 0");
+        cs_C2_C.setText("Charge: 0");
     }
 
    
@@ -279,29 +371,212 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
     
 
 
+    /**
+     * resets all the variables in tab 2
+     * @param event 
+     */
     @FXML
     private void cpClearBtnPressed(ActionEvent event) {
         b4_slider.setValue(20);
-        b4_tf.setText(null);
+        b4_tf.setText("10.00");
         cp_C1_slider.setValue(20);
-        cp_C1_tf.setText(null);
+        cp_C1_tf.setText("10.00");
         cp_C2_slider.setValue(20);
-        cp_C2_tf.setText(null);
+        cp_C2_tf.setText("10.00");
+        cp_C1_V.setText("Voltage: 0");
+        cp_C2_V.setText("Voltage: 0");
+        cp_C1_C.setText("Charge: 0");
+        cp_C2_C.setText("Charge: 0");
     }
 
+    /**
+     * adds either a branch or a resistor to the circuit
+     * @param event 
+     */
     @FXML
     private void rcAddBtnPressed(ActionEvent event) {
         
-        VBox testvb = resistorVbox;
-        testvb.setVisible(true);
-        rcSidebarVbox.getChildren().add(counter, helpText);
-        counter++;
+    if(rc_comboBox.getSelectionModel().getSelectedItem().equals("Resistor")) {
+    VBox copy = new VBox();
+    copy.setSpacing(resistorVbox.getSpacing());
+    copy.setAlignment(resistorVbox.getAlignment());
+
+    
+    for (Node n : resistorVbox.getChildren()) {
+
         
+            if (n instanceof Label originalLabel) {
+            if (originalLabel.getText().equals("Resistor #"+ counter1 + " Values:")) {
+                originalLabel.setText("Resistor #" + (counter1 + 1) +" Values:");
+            }
+            Label lbl = new Label(originalLabel.getText());
+            lbl.setFont(originalLabel.getFont());
+            lbl.setTextFill(originalLabel.getTextFill());
+            lbl.setStyle(originalLabel.getStyle());
+            copy.getChildren().add(lbl);
+        }
+            else if (n instanceof TextField originalTF) {
+                TextField tf = new TextField();
+                tf.setText(originalTF.getText());
+                tf.setPromptText(originalTF.getPromptText());
+                tf.setPrefWidth(originalTF.getPrefWidth());
+                tf.setStyle(originalTF.getStyle());
+                tf.setText("10.00");
+                copy.getChildren().add(tf);
+            }
+            else if (n instanceof Line originalLine) {
+                Line line = new Line();
+
+                line.setStartX(originalLine.getStartX());
+                line.setStartY(originalLine.getStartY());
+                line.setEndX(originalLine.getEndX());
+                line.setEndY(originalLine.getEndY());
+
+                line.setStroke(originalLine.getStroke());
+                line.setStrokeWidth(originalLine.getStrokeWidth());
+                line.setStyle(originalLine.getStyle());
+
+        copy.getChildren().add(line);
+            }
         
         
     }
+
+    copy.setVisible(true);
+
+    
+    rcSidebarVbox.getChildren().add(10 + counter1, copy);
+    rcvbs.add(counter1,copy);
+
+    counter1++;
+    }
+    }
+    
+    /**
+     * adds either a branch or a capacitor to the circuit
+     * @param event 
+     */
+    @FXML
+    private void ccAddBtnPressed(ActionEvent event) {
+        if(cc_comboBox.getSelectionModel().getSelectedItem().equals("Capacitor")) {
+    VBox copy = new VBox();
+    copy.setSpacing(capacitorvb.getSpacing());
+    copy.setAlignment(capacitorvb.getAlignment());
+
+    
+    for (Node n : capacitorvb.getChildren()) {
+
+        
+            if (n instanceof Label originalLabel) {
+            if (originalLabel.getText().equals("Capacitor #"+ counter2 + " Values:")) {
+                originalLabel.setText("Capacitor #" + (counter2 + 1) +" Values:");
+            }
+            Label lbl = new Label(originalLabel.getText());
+            lbl.setFont(originalLabel.getFont());
+            lbl.setTextFill(originalLabel.getTextFill());
+            lbl.setStyle(originalLabel.getStyle());
+            copy.getChildren().add(lbl);
+        }
+            else if (n instanceof TextField originalTF) {
+                TextField tf = new TextField();
+                tf.setText(originalTF.getText());
+                tf.setPromptText(originalTF.getPromptText());
+                tf.setPrefWidth(originalTF.getPrefWidth());
+                tf.setStyle(originalTF.getStyle());
+                tf.setText("10.00");
+                copy.getChildren().add(tf);
+            }
+            else if (n instanceof Line originalLine) {
+                Line line = new Line();
+
+                line.setStartX(originalLine.getStartX());
+                line.setStartY(originalLine.getStartY());
+                line.setEndX(originalLine.getEndX());
+                line.setEndY(originalLine.getEndY());
+
+                line.setStroke(originalLine.getStroke());
+                line.setStrokeWidth(originalLine.getStrokeWidth());
+                line.setStyle(originalLine.getStyle());
+
+        copy.getChildren().add(line);
+            }
+        
+        
+    }
+
+    copy.setVisible(true);
+
+    
+    ccSidebarVbox.getChildren().add(10 + counter2, copy);
+    ccvbs.add(counter2,copy);
+
+    counter2++;
+    }
+        else {
+            VBox copy = new VBox();
+    copy.setSpacing(capacitorvb.getSpacing());
+    copy.setAlignment(capacitorvb.getAlignment());
+
+    
+    for (Node n : capacitorvb.getChildren()) {
+
+        
+            if (n instanceof Label originalLabel) {
+            if (originalLabel.getText().equals("Capacitor #"+ counter2 + " Values:")) {
+                originalLabel.setText("Capacitor #" + (counter2 + 1) +" Values:");
+            }
+            Label lbl = new Label(originalLabel.getText());
+            lbl.setFont(originalLabel.getFont());
+            lbl.setTextFill(originalLabel.getTextFill());
+            lbl.setStyle(originalLabel.getStyle());
+            copy.getChildren().add(lbl);
+        }
+            else if (n instanceof TextField originalTF) {
+                TextField tf = new TextField();
+                tf.setText(originalTF.getText());
+                tf.setPromptText(originalTF.getPromptText());
+                tf.setPrefWidth(originalTF.getPrefWidth());
+                tf.setStyle(originalTF.getStyle());
+                tf.setText("10.00");
+                copy.getChildren().add(tf);
+            }
+            else if (n instanceof Line originalLine) {
+                Line line = new Line();
+
+                line.setStartX(originalLine.getStartX());
+                line.setStartY(originalLine.getStartY());
+                line.setEndX(originalLine.getEndX());
+                line.setEndY(originalLine.getEndY());
+
+                line.setStroke(originalLine.getStroke());
+                line.setStrokeWidth(originalLine.getStrokeWidth());
+                line.setStyle(originalLine.getStyle());
+
+        copy.getChildren().add(line);
+            }
+        
+        
+    }
+
+    copy.setVisible(true);
+
+    
+    ccSidebarVbox.getChildren().add(10 + counter2, copy);
+    ccvbs.add(counter2,copy);
+
+    counter2++;
+        }
+        
+        
+    }
+    
+    /**
+     * binds each slider to its corresponding textfield
+     * @param slider the slider to be binded
+     * @param textField the textfield to be binded
+     */
     private void bindSliderToTextField(Slider slider, TextField textField) {
-         textField.addEventFilter(KeyEvent.KEY_TYPED, event -> {
+        textField.addEventFilter(KeyEvent.KEY_TYPED, event -> {
         String ch = event.getCharacter();
 
         
@@ -336,6 +611,39 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
         }
     });
 }
+
+    @FXML
+    private void rcCalcBtnPressed(ActionEvent event) {
+        
+        String str = ((TextField)rcvbs.get(0).getChildren().get(2)).getText();
+        ((Label)rcvbs.get(0).getChildren().get(3)).setText(str);
+    }
+    @FXML
+    private void rcClearBtnPressed(ActionEvent event) {
+        b5_tf.setText("10.00");
+        b5_slider.setValue(0);
+        for(int i = 0; i < counter1;i++) {
+            ((TextField)rcvbs.get(i).getChildren().get(2)).setText("10.00");
+        }
+        
+        
+    }
+
+    @FXML
+    private void ccCalcBtnPressed(ActionEvent event) {
+      
+    }
+
+    @FXML
+    private void ccClearBtnPrssed(ActionEvent event) {
+        b6_tf.setText("10.00");
+        b6_slider.setValue(0);
+        for(int i = 0; i < counter1;i++) {
+            ((TextField)ccvbs.get(i).getChildren().get(2)).setText("10.00");
+    }
+    }
+    
+    
 
 /**
      * UPDATE THIS I CAN'T TYPE IT RIGHT NOW IM SO TIRED
@@ -410,7 +718,7 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
                 calcResistance(calcCircuit, circuitSize - 1, equResistor, isSplit, splitResistance);
                 ((Resistor)calcCircuit[circuitSize - 1]).setCurrent(((Resistor)calcCircuit[circuitSize - 2]).getCurrent());
                 ((Resistor)calcCircuit[circuitSize - 1]).setResistance(store);
-                ((Resistor)calcCircuit[circuitSize - 1]).setVoltage(((Resistor)calcCircuit[circuitSize - 1]).getCurrent() * ((Resistor) calcCircuit[1]).getResistance());
+                ((Resistor)calcCircuit[circuitSize - 1]).setVoltage(((Resistor)calcCircuit[circuitSize - 1]).getCurrent() * ((Resistor) calcCircuit[circuitSize - 1]).getResistance());
             }
             //if the 2nd to next element is a circuitSplit
             else {
@@ -419,7 +727,4 @@ public class ElectricalCircuitSimulationFXMLController implements Initializable 
         }
     }   
 
-    
-    
-   
 }
